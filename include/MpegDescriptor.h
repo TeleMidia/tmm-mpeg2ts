8c3979f61a3cf90589e2cfb9b9fb8c2a356fa0f9
Felippe Nagato <fnagato@gmail.com>
2016-12-01 22:50:25 -0200

    Adding GNU Affero General Public License header.

--- include/MpegDescriptor.h
+++ include/MpegDescriptor.h
@@ -1,49 +1,17 @@
 /******************************************************************************
-Este arquivo eh parte da implementacao das ferramentas DTV do TeleMidia
+TeleMídia Multiplexer MPEG-2 Transport Stream library
+Copyright (C) 2016 TeleMídia Lab/PUC-Rio
+https://github.com/TeleMidia/tmm-mpeg2ts/graphs/contributors
 
-Direitos Autorais Reservados (c) 1989-2007 PUC-Rio/Laboratorio TeleMidia
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version. This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+for more details. You should have received a copy of the GNU Affero General
+Public License along with this program. If not, see http://www.gnu.org/licenses/.
 
-Este programa eh software livre; voce pode redistribui-lo e/ou modificah-lo sob
-os termos da Licenca Publica Geral GNU versao 2 conforme publicada pela Free
-Software Foundation.
-
-Este programa eh distribuido na expectativa de que seja util, porem, SEM
-NENHUMA GARANTIA; nem mesmo a garantia implicita de COMERCIABILIDADE OU
-ADEQUACAO A UMA FINALIDADE ESPECIFICA. Consulte a Licenca Publica Geral do
-GNU versao 2 para mais detalhes.
-
-Voce deve ter recebido uma copia da Licenca Publica Geral do GNU versao 2 junto
-com este programa; se nao, escreva para a Free Software Foundation, Inc., no
-endereco 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
-
-Para maiores informacoes:
-ncl @ telemidia.puc-rio.br
-http://www.ncl.org.br
-http://www.ginga.org.br
-http://www.telemidia.puc-rio.br
-******************************************************************************
-This file is part of the TeleMidia DTV Tools
-
-Copyright: 1989-2007 PUC-RIO/LABORATORIO TELEMIDIA, All Rights Reserved.
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License version 2 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more
-details.
-
-You should have received a copy of the GNU General Public License version 2
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
-For further information contact:
-ncl @ telemidia.puc-rio.br
-http://www.ncl.org.br
-http://www.ginga.org.br
-http://www.telemidia.puc-rio.br
 *******************************************************************************/
 
 #ifndef MPEGDESCRIPTOR_H_
--- src/dsmcc/carousel/Binding.cpp
+++ src/dsmcc/carousel/Binding.cpp
@@ -1,3 +1,19 @@
+/******************************************************************************
+TeleMídia Multiplexer MPEG-2 Transport Stream library
+Copyright (C) 2016 TeleMídia Lab/PUC-Rio
+https://github.com/TeleMidia/tmm-mpeg2ts/graphs/contributors
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version. This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+for more details. You should have received a copy of the GNU Affero General
+Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
+*******************************************************************************/
+
 /*
  * Binding.cpp
  *
